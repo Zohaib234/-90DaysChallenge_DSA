9cebe593b99c08b830324b600abc592744a7f65b
Rafay Shakeel <114006513+thePunchBox@users.noreply.github.com>
2022-12-06 11:56:55 +0000

    commit

---
+++ Rafay Shakeel 21SW152/String/ValidPalindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class ValidPalindrome{
+    /**
+     * @param args
+     */
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a word to be checked as a palindrome: ");
+        String word= sc.nextLine();
+
+        final boolean notIsPalindrome(word){
+            System.out.println("True");
+        }
+
+    }
+}
+
+//Still Full Of Errors
